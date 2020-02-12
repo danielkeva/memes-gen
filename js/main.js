3ec99832e7c91ea4f402392f11485916c9665cec
Daniel Keva <danielkeva@Daniels-MacBook-Pro.local>
2020-02-12 15:51:31 +0200

    phase1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,67 @@
 'use strict';
-console.log('main')
+
+var gCanvas;
+var gCtx;
+function onInit() {
+
+    gCanvas = document.getElementById('my-canvas')
+    gCtx = gCanvas.getContext('2d')
+    drawOnCanvas()
+    renderGallery()
+}
+
+function drawOnCanvas() {
+    onDrawImg()
+
+
+}
+
+
+function onDrawImg(txt) {
+    var img = new Image()
+    img.src = getSelectedImg()
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height)
+        if (!txt) return
+        drawText(txt, gCanvas.width / 2, 60)
+
+    }
+}
+
+
+function drawText(text, x, y) {
+    // gCtx.restore();
+    gCtx.lineWidth = '3'
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '5 0px Impact'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+    // gCtx.save();
+
+}
+
+function onUpdateLine(txt) {
+    updateLine(txt)
+    gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+    onDrawImg(txt)
+
+
+}
+
+function renderGallery() {
+    var imgs = getImgsToDisplay()
+
+    var strHTMLs = imgs.map(img => {
+        return `
+     <img onclick="onUpdateSelectedImg(${img.id})" src="${img.url}" >`
+    })
+    var elGalleryContainer = document.querySelector('.gallery-container')
+    elGalleryContainer.innerHTML = strHTMLs.join('')
+}
+
+function onUpdateSelectedImg(imgId){
+    updateSelectedImg(imgId)
+    onDrawImg()
+}
--- js/service.js
+++ js/service.js
@@ -1,2 +1,54 @@
 'use strict';
-console.log('service')
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: null,
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
+var gImgs = createImgs()
+
+
+
+
+function getSelectedImg() {
+    var img = gImgs.find(img => {
+        return img.id === gMeme.selectedImgId
+    })
+    return img.url
+}
+
+
+function getTxtToDisplay() {
+    return gMeme.lines[gMeme.selectedLineIdx].txt
+}
+
+function updateLine(txt) {
+    gMeme.lines[gMeme.selectedLineIdx].txt = txt
+}
+
+function createImgs() {
+    var imgs = []
+    var id = 0
+    for (var i = 0; i < 2; i++) {
+        imgs.push(
+            { id: ++id, url: `img/${id}.jpg` }
+        )
+    }
+    return imgs
+}
+
+function getImgsToDisplay() {
+    return gImgs
+}
+
+function updateSelectedImg(imgId) {
+    gMeme.selectedImgId = imgId
+    
+}
